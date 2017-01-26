dcb1c9069112fe6caf2e31d85da76b53aa919374
Maximilian Bode <maximilian.bode@tngtech.com>
2017-01-26 19:23:53 +0100

    Only enable zsh if it is not already set.
    Do not prompt for password if not necessary.

---
+++ zsh.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+if [ "$(which zsh)" = "$SHELL" ]; then
+    echo "zsh already installed"
+else
+    echo "zsh not installed"
+    echo "Adding zsh to whitelisted system shells"
+    sudo grep -q -F $(which zsh) /etc/shells || sudo -s echo $(which zsh) >> /etc/shells
+    echo "Changing login shell to zsh"
+    chsh -s $(which zsh)
+fi;
