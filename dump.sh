6f691f3d83ecd8bdd089ee8c0464d053eff220ce
Maximilian Bode <maximilian.bode@tngtech.com>
2017-07-31 07:36:50 +0200

    Make python 2 requirements explicit

--- dump.sh
+++ dump.sh
@@ -3,8 +3,8 @@
 echo "Dumping homebrew..."
 brew bundle dump --force
 
-echo "Dumping pip..."
-pip freeze > requirements.txt
+echo "Dumping pip2..."
+pip2 freeze > requirements2.txt
 
 echo "Dumping pip3..."
 pip3 freeze > requirements3.txt
