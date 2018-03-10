c37fc0b65ad457174896e88e98aefc3c5a751ef6
Maximilian Bode <maximilian.bode@tngtech.com>
2018-03-10 19:25:33 +0100

    Fix SC2046

--- brew.sh
+++ brew.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-if test ! $(which brew); then
+if test ! "$(which brew)"; then
   echo "Installing homebrew..."
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
--- nix.sh
+++ nix.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env sh
 
-if test ! $(which nix); then
+if test ! "$(which nix)"; then
   echo "Installing nix..."
   curl https://nixos.org/nix/install | sh
 fi
