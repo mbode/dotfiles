23f971a8801884b1a0191faa34fcce3349e3f99c
Maximilian Bode <maximilian.bode@tngtech.com>
2018-03-11 13:20:01 +0100

    Source nix-daemon after install, add nix to path

--- nix.sh
+++ nix.sh
@@ -3,4 +3,6 @@
 if test ! "$(command -v nix)"; then
   echo "Installing nix..."
   curl https://nixos.org/nix/install | sh
+  # shellcheck disable=SC1091
+  . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
 fi
