3f11bb8cc028f5c14d9971c7e2ebfa86522760f1
Maximilian Bode <maximilian.bode@tngtech.com>
2018-06-30 19:10:16 +0200

    Use dotbot-brew to install bundle from Brewfile

--- brew.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-
-if test ! "$(command -v brew)"; then
-  echo "Installing homebrew..."
-  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
